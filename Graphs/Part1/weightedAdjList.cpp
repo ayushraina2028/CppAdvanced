6efd31aa60a7225e4414d2a5a84a01227b7313d5
ayushraina2028 <ayushraina@iisc.ac.in>
2023-11-10 12:38:37 +0530

    weighted Adj List

--- Graphs/Part1/weightedAdjList.cpp
+++ Graphs/Part1/weightedAdjList.cpp
@@ -3,21 +3,21 @@
 #include <list>
 
 using namespace std;
+
 vector<list<pair<int, int>>> graph;
 
 void add_edge(int source, int destination, int weight, bool dir = true) {
     graph[source].push_back({destination, weight});
 
     if(dir) {
-        graph[destination].push_back({source,weight});
+        graph[destination].push_back({source, weight});
     }
 }
 
 void display() {
     for(int i = 0;i < graph.size(); i++) {
-        cout << i << " -> ";
-        for(auto array : graph[i]) {
-            cout << "(" << array.first << "," << array.second << ")";
+        for(auto pair : graph[i]) {
+            cout << "(" << i << " -> " << pair.first  << ") : " << pair.second << "   "; 
         }
         cout << endl;
     }
@@ -25,12 +25,10 @@ void display() {
 
 int main() {
     int vertices;
-    cin >> vertices;
-
-    graph.resize(vertices, list<pair<int, int>> ());
-
     int edges;
-    cin >> edges;
+    cin >> vertices >> edges;
+
+    graph.resize(vertices+1, list<pair<int, int>> ());
 
     while(edges--) {
         int source, destination, weight;
